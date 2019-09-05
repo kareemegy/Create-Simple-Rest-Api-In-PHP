f110dfe249cf5352da45414dd04d16a51ccb8ef6
karim <kokekarim@yahoo.com>
2019-09-05 15:18:50 +0200

    post_json_data

--- objects/product.php
+++ objects/product.php
@@ -32,7 +32,7 @@ class Product
                     categories c
                         ON p.category_id = c.id
             ORDER BY
-                p.created DES";
+                p.created ASC";
 
         // prepare query statement
         $stmt = $this->conn->prepare($query);
@@ -41,6 +41,41 @@ class Product
         $stmt->execute();
 
         return $stmt;
+    }
+    // create product
+    public function create()
+    {
+
+        // query to insert record
+        $query = "INSERT INTO
+                " . $this->table_name . "
+            SET
+                name=:name, price=:price, description=:description, category_id=:category_id, created=:created";
+
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+
+        // sanitize
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->price = htmlspecialchars(strip_tags($this->price));
+        $this->description = htmlspecialchars(strip_tags($this->description));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+        $this->created = htmlspecialchars(strip_tags($this->created));
+
+        // bind values
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":price", $this->price);
+        $stmt->bindParam(":description", $this->description);
+        $stmt->bindParam(":category_id", $this->category_id);
+        $stmt->bindParam(":created", $this->created);
+
+        // execute query
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+
     }
 
 }
